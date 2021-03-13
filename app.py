51d6c5052a5065831f0b23b811492b12c57e353d
MurodTuraev <tml.murod@mail.ru>
2021-03-13 22:20:01 +0500

    db

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Register(db.Model):
+    username = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    password = db.Column(db.String(80))
+    fname = db.Column(db.String(80))
+    lname = db.Column(db.String(80))
 
 
 @app.route('/')
