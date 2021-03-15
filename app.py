55d830f116c199e56d542877f3e0a76ac253d530
Murod Turaev <m.turaev@uzagro.agros>
2021-03-15 15:04:12 +0500

    register form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -16,8 +16,30 @@ class Register(db.Model):
 
 @app.route('/')
 def home():
-    return 'ok'
+    return render_template('index.html')
 
-
+@app.route('/signup')
+def signup():
+    r=request.values
+    uname=r.get('uname',None)
+    email=r.get('email',None)
+    pas=r.get('pass',None)
+    fname=r.get('fname',None)
+    lname=r.get('lname',None)
+    user=Register(username=uname,email=email,password=pas,fname=fname,lname=lname)
+    users=Register.query.all()
+    # total=f'Username: {users.username}'
+    db.session.add(user)
+    db.session.commit()
+    for i in users:
+        total=f''' Succesfully registerid!!!</p>
+        Username: {i.username}
+        <p>Email: {i.email}</p>
+        <p>Password: {i.password}</p>
+        <p>First name: {i.fname}</p>
+        <p>Last Name: {i.lname}'''
+        return total
+    
+    
 if __name__ == '__main__':
     app.run(debug=True)
